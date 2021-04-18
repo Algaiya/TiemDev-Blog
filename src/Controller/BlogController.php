df8e2831cf253ba2ec64415404907dc32e0205b0
Tiemoko Diallo <71785732+Algaiya@users.noreply.github.com>
2021-04-18 20:50:41 +0200

    Commit lié aux nombreuses améliorations apportées au TP la journée du  18/04

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -23,5 +28,35 @@ class BlogController extends AbstractController
      */
     public function home() {
         return $this->render('blog/home.html.twig');
+    }
+
+    /**
+     * @Route("/blog/article{id}", name="blog_show")
+     */
+    public function show(Article $article) {
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/Animaux", name="blog_animals")
+     */
+    public function animaux() {
+        return $this->render('blog/animaux.html.twig');
+    }
+
+    /**
+     * @Route("/Enfant", name="blog_kid")
+     */
+    public function enfant() {
+        return $this->render('blog/enfant.html.twig');
+    }
+
+    /**
+     * @Route("/Decoration", name="blog_decoration")
+     */
+    public function decoration() {
+        return $this->render('blog/decoration.html.twig');
     }
 }
